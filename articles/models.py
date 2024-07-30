f0503ab23da26b87f633b3829a67b111c70702a1
jumi <ajsel422@gmail.com>
2024-07-30 10:40:56 +0900

    model 정의

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # auto_now_add 자동으로 현재시점 추가 기본값은 False
